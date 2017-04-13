f50e2d5c884d633caf7f2ad8db9cda94f01f27bd
kyleliang919 <kyleliang919@gmail.com>
2017-04-13 01:33:20 -0500

    Create README.m

---
+++ HW7/README.m
@@ -0,0 +1,14 @@
+To run the classification with 10 perceptrons, run command python main.py in the terminal.
+To run the classification with neural network, run command python NN.py in the terminal.
+Files:
+	main.py: include sthe code of data reading and multiclass_perceptrons training and testing
+	multiclass_perceptron.py: includes code for the initialization, training and testing of multiple perceptrons
+	perceptron.py: includes the code of the implementation of the single perceptron class
+	NN.py: includes code of the neural network(DNNClassifier)
+Package information:
+	Python 3.5
+	numpy 1.12.0
+	tensorflow 1.0
+Author:Kaizhao Liang (kl2)
+Reference:
+https://www.tensorflow.org/
